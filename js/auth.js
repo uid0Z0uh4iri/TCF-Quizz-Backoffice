14f03ba42db3d286a0c26932cbde5e8a5f1f0a47
Mohamed Karroumi <mkaroumi123@gmail.com>
2024-11-13 09:20:35 +0100

    QB-4 starting to implement user validation for test

--- js/auth.js
+++ js/auth.js
@@ -1,16 +1,7 @@
-
 let user = null;
-
-
 
 user = localStorage.getItem('currentUser') ? JSON.parse(localStorage.getItem('currentUser')) : null;
 if (!user) {
-    window.location.href = 'login.html';
-}
-
-
-function logout() {
-    localStorage.removeItem('currentUser');
     window.location.href = 'login.html';
 }
 
